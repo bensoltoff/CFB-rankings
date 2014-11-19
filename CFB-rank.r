de958711118c1cea4366039d7131b7d548f5be4a
bensoltoff <ben@bensoltoff.com>
2014-11-19 09:28:16 -0500

    initial commit
    rank CFB teams as of current weak in 2014 season

---
+++ CFB-rank.r
@@ -0,0 +1,93 @@
+## CFB-rank.r
+## 11/17/14 BCS
+## Automatically rank college football teams based upon random preseason ranking
+
+rm(list=ls())
+
+
+require(plyr)
+require(dplyr)
+require(ggplot2)
+require(tidyr)
+require(snowfall)
+
+set.seed(11091987)
+
+
+# read in functions for ranking generation
+source(file="functions.r")
+
+# parameters to set
+nsims <- 1000      # number of simulations to conduct for each season
+ncpu <- 6         # number of available CPUs for parallel processing
+
+# read in game outcomes (from Football-Ref)
+games <- read.csv(file="data//cfb_games.csv", stringsAsFactors=FALSE)
+games <- tbl_df(games)
+
+# read in teams (from Wikipedia)
+teams <- read.csv(file="data//teams.csv", stringsAsFactors=FALSE)
+teams <- tbl_df(teams)
+
+# merge FCS teams that play an FBS school into teams data frame
+teams <- games %>%
+  select(school, opponent) %>%
+  gather(homeaway, school, school, opponent) %>%
+  select(school) %>%
+  unique() %>%
+  left_join(teams) %>%
+  mutate(team_id = row_number())
+teams
+
+# replace school and opponent names with team id number
+games <- games %>%
+  left_join(select(teams, school, team_id)) %>%
+  mutate(team1id=team_id) %>%
+  select(-team_id) %>%
+  left_join(rename(select(teams, school, team_id), opponent=school)) %>%
+  mutate(team2id=team_id) %>%
+  select(-team_id, -opponent, -school, -id) %>%
+  arrange(date, gamenum, team1id, team2id)
+games
+
+# correct dates format
+games <- games %>%
+  mutate(date = as.numeric(strftime(as.Date(date,"%m/%d/%Y"), format = "%j")))
+games
+
+# identify in which week each game was play
+game.weeks <- 16      # number of weeks in which regular season games are played
+weeks <- data.frame(date=seq(from=as.numeric(strftime(as.Date("08/26/2014","%m/%d/%Y"), format = "%j")),
+                             length.out=(7*game.weeks)),
+                    week=sort(rep(1:game.weeks, times=7)))
+weeks <- tbl_df(weeks)
+
+games <- games %>%
+  left_join(weeks)
+
+# calculate who won the game
+games <- games %>%
+  mutate(outcome=(outcome=="W"), winner=(team1id*(outcome==TRUE) + team2id*(outcome==FALSE)),
+         loser=(team1id*(outcome==FALSE) + team2id*(outcome==TRUE))) %>%
+  select(date, week, winner, loser)
+games
+ 
+
+# simulate rankings with multiple pre-season rankings
+# system.time(ranks <- rlply(nsims, rank.season(games, preseason.rank(teams)), .progress="text"))
+
+# parallel method
+sfInit(parallel = TRUE, cpus = ncpu, type = "SOCK")
+sfExportAll() #export appropriate objects that will be needed inside a function, if applicable
+sfSource(file="functions.r")
+sfLibrary(snowfall)
+sfLibrary(plyr)
+sfLibrary(dplyr)
+sfLibrary(tidyr)
+system.time(ranks <- sfSapply(1:nsims, function(x) rank.season(games, preseason.rank(teams)), simplify = FALSE))
+sfStop()
+
+
+save.image(file="output//CFB-sim.RData")
+
+
---
+++ functions.r
@@ -0,0 +1,77 @@
+## functions.r
+## 11/19/14 BCS
+## Functions necessary for CFB ranking generation
+
+
+# randomly rank teams in preseason
+preseason.rank <- function(teams){
+  # randomly rank all FBS teams
+  fbs <- teams %>%
+    filter(!is.na(conference)) %>%
+    sample_frac()
+  
+  # randomly rank all FCS teams
+  fcs <- teams %>%
+    filter(is.na(conference)) %>%
+    sample_frac()
+  
+  # combine ranks, all FBS teams ranked higher than FCS
+  teams <- rbind(fbs, fcs) %>%
+    mutate(rank=row_number(), week=0)
+  
+  return(teams)
+}
+
+# calculate new rank based upon game result
+new.rank <- function(game, ranks) {
+  # reshape to two observations and merge current rank
+  game <- gather(game, outcome, team_id, winner:loser) %>%
+    left_join(ranks, by="team_id")
+  
+  # if the loser is higher ranked, split the difference between the teams' previous ranks
+  # else do nothing and keep the existing ranks
+  if(select(filter(game, outcome=="winner"), rank) < select(filter(game, outcome=="loser"), rank)){
+    # calculate mean rank of two teams
+    mean.rank <- as.numeric(summarize(filter(ranks, team_id %in% game$team_id),mean=mean(rank)))
+    
+    ranks$rank[ranks$team_id==game$team_id[game$outcome=="winner"]] <- mean.rank+.1
+    ranks$rank[ranks$team_id==game$team_id[game$outcome=="loser"]] <- mean.rank-.1
+  }
+  
+  return(ranks)
+}
+
+# calculate new rank for each week
+rank.week <- function(games, ranks, week.num){
+  games <- filter(games, week==week.num)
+  
+  # only keep ranks from previous week
+  current.ranks <- ranks %>%
+    filter(week==(week.num-1)) %>%
+    select(-week)
+  
+  # for each game, calculate new rank and replace pre.ranks with new ranks
+  for(i in 1:nrow(games)){
+    current.ranks <- new.rank(games[i,], current.ranks)
+  }
+  
+  return(current.ranks)
+}
+
+# calculate new rank for each season
+rank.season <- function(games, pre.ranks){
+  # drop unnecessary variables from ranks
+  pre.ranks <- select(pre.ranks, school, conference, team_id, rank, week)
+  current.ranks <- pre.ranks
+  
+  # for each week, calculate new rank and store in data.frame
+  overall.ranks <- pre.ranks
+  
+  for(i in unique(games$week)){
+    current.ranks <- rank.week(games, current.ranks, i) %>%
+      mutate(week=i)
+    overall.ranks <- rbind(overall.ranks, current.ranks)
+  }
+  
+  return(overall.ranks)
+}
